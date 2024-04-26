30097cb9200a2bb866b86a866e155e6de67b0d2b
r3stl3sscyph3r <166079141+r3stl3sscyph3r@users.noreply.github.com>
2024-04-25 21:33:12 -0700

    working on lab 5

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,17 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - lab 5
+// Author: Salem Hunstable
+// Date: 04/25/24
 
-// Constants
-
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+  //variables
+make = "Nissan";
+model = "Rogue";
+color = "Black";
+year = 2015;
+age = 2024- year;
+  
+//output 
+document.writeln('Make: '+ make + "<br>"); 
+document.writeln('Model: '+ model + "<br>"); 
+document.writeln('Color: '+ color + "<br>"); 
+document.writeln('Year: '+ year + "<br>"); 
+document.writeln('Age: '+ age + "<br>"); 
