48c8fa982b2b77c475a318e14a5dc5c706a5db67
r3stl3sscyph3r <166079141+r3stl3sscyph3r@users.noreply.github.com>
2024-05-06 19:01:40 -0700

    asdfghjk

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -13,8 +13,8 @@ console.log("3 squared: ",squareIt(3));
 array = [5,3,7,25,32]
 console.log("my array: ",array);
 
-var result = array.map(squareIt);
-console.log("squared array: ", result);
+var square = array.map(squareIt);
+console.log("squared array: ", square);
 
 var result = array.map(function(x){
     return x + 2;
@@ -24,9 +24,9 @@ var mapResults = array.map(squareIt);
 console.log("Results: ", mapResults);
 
  // Your map results data
- var display = array + result + mapResults;
+ var display = array;
 
  // Use jQuery to select the element by its ID and set the HTML content
- $("#output").html(display);
+ $("#output").html("Original array:", display);
  
  
