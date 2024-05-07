fb4bef1e9b488ac8ee6afc5c9131edfa44dbc87c
r3stl3sscyph3r <166079141+r3stl3sscyph3r@users.noreply.github.com>
2024-05-06 19:06:06 -0700

    asdfghjkl;

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -24,9 +24,9 @@ var mapResults = array.map(squareIt);
 console.log("Results: ", mapResults);
 
  // Your map results data
- //var display = array;
+ var display = array;
 
  // Use jQuery to select the element by its ID and set the HTML content
- $("#output").html("Original array:", array);
+ $("#output").html("Original array:", display);
  
  
