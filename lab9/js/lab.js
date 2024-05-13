92d37f5205913b7c0295c4e345d6983d3dd978f6
r3stl3sscyph3r <166079141+r3stl3sscyph3r@users.noreply.github.com>
2024-05-13 00:08:56 -0700

    ASDFGHJKL

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -6,11 +6,23 @@
    Author: Salem
    Date: 2023
 */
-// add button to challenge section
+// Add buttons to challenge, problems, and results sections
 $("#challenge").append("<button id='button-challenge'>Make Special</button>");
+$("#problems").append("<button id='button-problems'>Make Special</button>");
+$("#results").append("<button id='button-results'>Make Special</button>");
 
-// add a click listener to the challenge button
-$("button-challenge").click(function(){
-    // now add (or subtract) the "special" class to the section
+// Add click listeners to the buttons
+$("#button-challenge").click(function(){
+    // Toggle the "special" class for the challenge section
     $("#challenge").toggleClass("special");
+});
+
+$("#button-problems").click(function(){
+    // Toggle the "special" class for the problems section
+    $("#problems").toggleClass("special");
+});
+
+$("#button-results").click(function(){
+    // Toggle the "special" class for the results section
+    $("#results").toggleClass("special");
 });
