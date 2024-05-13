3eeda5257d9c05dbe3bf4f3b45bbbe6a933b4a9c
r3stl3sscyph3r <166079141+r3stl3sscyph3r@users.noreply.github.com>
2024-05-13 00:50:41 -0700

    asdfghjkl;

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -9,11 +9,9 @@
 
 
 // Add buttons to challenge, problems, and results sections
-function pretty(){
-    $("#challenge").toggleClass("special")
-  }
-  //click button
-  $("#button-challenge").click(pretty)
-  
-
-
+$("#challenge").append("<button id='button-challenge'>Make Special</button>");
+// add a click listener to the challenge button
+$("button-challenge").click(function(){
+    // now add (or subtract) the "special" class to the section
+    $("#challenge").toggleClass("special");
+});
