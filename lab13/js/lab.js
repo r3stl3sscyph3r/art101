ceff499333d2572bebf959e25e751cd8033dd8fc
r3stl3sscyph3r <166079141+r3stl3sscyph3r@users.noreply.github.com>
2024-05-12 22:07:04 -0700

    asdfghjkl;

---
+++ lab13/js/lab.js
@@ -0,0 +1,24 @@
+// index.js - lab 8
+// Author: salem hunstable
+// Date: 5/06/24
+
+function squareIt(x){
+    return x * x;
+}
+
+console.log("5 squared: ",squareIt(5));
+console.log("3 squared: ",squareIt(3));
+
+array = [5,3,7,25,32]
+console.log("my array: ",array);
+
+var result = array.map(squareIt);
+console.log("squared array: ", result);
+
+var result = array.map(function(x){
+    return x*2;
+})
+console.log("array again: ", result); 
+
+
+
