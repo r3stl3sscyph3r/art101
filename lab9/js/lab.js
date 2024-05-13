948096ed25716ae3b43a79e729269ee1c3e6ed10
r3stl3sscyph3r <166079141+r3stl3sscyph3r@users.noreply.github.com>
2024-05-12 22:52:06 -0700

    asdfghjkl;

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -10,7 +10,7 @@
 $("#challenge").append("<button id='button-challenge'>Make Special</button>");
 
 // add a click listener to the challenge button
-$('button-challenge').click(function(){
+$("button-challenge").click(function(){
     // now add (or subtract) the "special" class to the section
     $("#challenge").toggleClass("special");
 });
