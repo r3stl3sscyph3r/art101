f186c9aea446468887cf787142b03277c0035899
r3stl3sscyph3r <166079141+r3stl3sscyph3r@users.noreply.github.com>
2024-06-06 22:30:41 -0700

    vfjvfkvbkvf fvk

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -2,23 +2,33 @@
 // Author: salem hunstable
 // Date: 5/06/24
 
-function squareIt(x){
-    return x * x;
-}
-
-console.log("5 squared: ",squareIt(5));
-console.log("3 squared: ",squareIt(3));
-
-array = [5,3,7,25,32]
-console.log("my array: ",array);
-
-var result = array.map(squareIt);
-console.log("squared array: ", result);
-
-var result = array.map(function(x){
-    return x*2;
+// Using the core $.ajax() method
+$.ajax({
+    // The URL for the request (from the api docs)
+    url: "https://yourapiendpoint.com/",
+    // The data to send (will be converted to a query string)
+    data: { 
+            // here is where any data required by the api 
+            //   goes (check the api docs)
+            id: 123,
+            api_key: "blahblahblah",
+          },
+    // Whether this is a POST or GET request
+    type: "GET",
+    // The type of data we expect back
+    dataType : "json",
+    // What do we do when the api call is successful
+    //   all the action goes in here
+    success: function(data) {
+        // do stuff
+        console.log(data);
+    },
+    // What we do if the api call fails
+    error: function (jqXHR, textStatus, errorThrown) { 
+        // do stuff
+        console.log("Error:", textStatus, errorThrown);
+    }
 })
-console.log("array again: ", result); 
 
 
 
